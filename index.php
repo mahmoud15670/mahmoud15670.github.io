a5b7b06f5f67ab2e0c36dec448226167c65f69fc
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2023-06-09 17:09:28 +0000

    hash

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php each 'mamhoud'
+<?php echo("<!DOCTYPE html>");
+echo("<html lang='en'>");
+echo("<head>");
+echo("<title>Mahmoud Ghallab</title>");
+echo("</head>");
+echo("<body>");
+echo("mahmoud"); 
+echo("</body>");
+echo("</html>");
