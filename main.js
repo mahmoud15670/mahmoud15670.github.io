14fe1a38dc5c69a67a20121e51b5f039a791f1cb
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2023-06-16 15:57:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,16 +4,16 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('nav a[href="#certificates"]').addEventListener('click', move_certificates);
     document.querySelector('nav a[href="#contct"]').addEventListener('click', move_contct);
     window.addEventListener('scroll', () => {
-        if (window.scrollY >= 600 && window.scrollY <= 640) {
+        if (window.scrollY >= 600 && window.scrollY <= 630) {
             move_services()
         }
-        if (window.scrollY >= 1300 && window.scrollY <= 1340) {
+        if (window.scrollY >= 1300 && window.scrollY <= 1330) {
             move_projects()
         }
-        if (window.scrollY >= 2300 && window.scrollY <= 2340) {
+        if (window.scrollY >= 2300 && window.scrollY <= 2330) {
             move_certificates()
         }
-        if (window.scrollY >= 2900 && window.scrollY <= 2940) {
+        if (window.scrollY >= 2900 && window.scrollY <= 2930) {
             move_contct()
         }
     });
